e332e6ecc9dfddda20038380b8280d7b6ac63ef4
Joel Watson <joelwat@gmail.com>
2021-03-01 15:44:00 -0800

    Updates eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,67 @@
+const resolve = require('path').resolve;
+
 module.exports = {
   env: {
     browser: true,
     'jest/globals': true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:jest/recommended',
     'plugin:jest/style',
     '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended',
     'airbnb-typescript/base',
   ],
-  parser: '@typescript-eslint/parser',
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 2020,
-    project: 'tsconfig.json',
+    parser: '@typescript-eslint/parser',
+    project: resolve(`${__dirname}/tsconfig.json`),
+    extraFileExtensions: [
+      '.vue',
+    ],
   },
   plugins: [
-    'import',
-    'jest'
+    'jest',
   ],
   root: true,
   // add your custom rules here
-  rules: {},
-  settings: {
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {
-        alwaysTryTypes: true,
-        project: './tsconfig.json',
+  rules: {
+    'max-len': [
+      'error',
+      {
+        code: 120,
+        ignorePattern: 'd="[\\s\\S]*?"'
       }
-    }
-  }
-}
+    ]
+  },
+  settings: {
+  //   'import/parsers': {
+  //     '@typescript-eslint/parser': ['.ts', '.tsx'],
+  //   },
+    'import/resolver': {
+      [require.resolve('eslint-import-resolver-node')]: {},
+      [require.resolve('eslint-import-resolver-webpack')]: {
+        config: require.resolve('@vue/cli-service/webpack.config.js'),
+      },
+      'alias': {
+        map: [
+          [ '@/', 'src/'],
+          [ '~/', 'src/'],
+        ],
+        extensions: [ '.ts', '.vue'],
+      },
+    },
+    //   'import/extensions': [
+    //     '.js',
+    //     '.jsx',
+    //     '.mjs',
+    //     '.ts',
+    //     '.tsx',
+    //   ],
+  //     typescript: {
+  //       alwaysTryTypes: true,
+  //       project: './tsconfig.json',
+  //     },
+  },
+};
